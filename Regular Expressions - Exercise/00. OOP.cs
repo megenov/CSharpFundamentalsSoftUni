cf712fc097266af363af7d9a73a38072c86a3eff
Miroslav Genov <megenov@yahoo.com>
2021-03-25 16:04:57 +0200

    Create 00. OOP.cs

---
+++ Regular Expressions - Exercise/00. OOP.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _00._OOP
+{
+    class Car
+    {
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public int Year { get; set; }
+
+        //public Car(string modelName, string modelColor, int modelYear)
+        //{
+        //    Model = modelName;
+        //    Color = modelColor;
+        //    Year = modelYear;
+        //}
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Car ford = new Car();
+            //ford.Model = "Mustang";
+
+            //Car ford = new Car("Mustang", "Red", 1488);
+
+            Car ford = new Car { Model = "Mustang", Color = "Red", Year = 1488 };
+
+            Console.WriteLine(ford.Model);
+        }
+    }
+}
